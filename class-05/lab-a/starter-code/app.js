b7a37d28d3de56ac420a3a32d1e3aaefdafd55ab
GhofranAljabr <ghofran.gorro@gmail.com>
2021-06-11 02:21:57 +0300

    solve p3

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -50,11 +50,19 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  let ab = sum(a, b)[0];
+   let abc = sum(ab, c)[0];
+   let ab2 = multiply(a, b)[0];
+   let abc2 = multiply(ab2, c)[0];
+
+ return [abc, abc2, +a + " and " + b + " and " + c + " sum to " + abc + ".",
+  "The product of " + a + " and " + b + " and " + c + " is " + abc2 + "."];
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ 
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
